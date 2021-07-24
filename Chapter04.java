bccbb312134ad50076e88bd56e012964cad8d104
RIKO-tlov <bluebyrair@outlook.jp>
2021-07-24 12:14:29 +0900

    [finish]Chapter4

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 {
+
+    public static void main(String[] args) {
+        final int MIN_NUMBER = 2;
+        String str; 
+        str = "リコ"; 
+        System.out.println(str);
+
+        str = "鰻"; 
+        System.out.println(str); 
+        System.out.println(MIN_NUMBER);
+    }
+}
